f392fdb851b7c483e91849c537eca28c619109e5
Abdulsalam Abdikarim <achalam00@gmail.com>
2024-08-31 22:48:39 +0300

    Added target url for create google account

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import time
 service = Service(executable_path = "./chromedriver")
 driver = webdriver.Chrome(service = service)
 
-driver.get("https://google.com")
+driver.get("https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fmyaccount.google.com%2F%3Futm_source%3Dsign_in_no_continue%26pli%3D1&ec=GAlAwAE&hl=en&service=accountsettings&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S-1019797056%3A1725133534064740&ddm=0")
+
 time.sleep(10)
 
 driver.quit()
